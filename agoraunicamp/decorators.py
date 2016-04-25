cdb18290ca928f5ccc5ac451e022931a106a02a4
tiagonovaes <tiagonovaesangelo@gmail.com>
2016-04-25 15:59:14 -0300

    ldap

--- agoraunicamp/decorators.py
+++ agoraunicamp/decorators.py
@@ -30,7 +30,7 @@ def term_required(function):
             us = User.objects.get(user=request.user)
         except:
             u = UserSys.objects.get(username=request.user)
-            x = User(user=u, primeiro_nome=z.first(), ultimo_nome=result_data[0][1]['departmentNumber'], projeto="default")
+            x = User(user=u, primeiro_nome=z.values(), ultimo_nome=result_data[0][1]['departmentNumber'], projeto="default")
             x.save()
             us = User.objects.get(user=request.user)
         cond = Termo.objects.get(user=us)
