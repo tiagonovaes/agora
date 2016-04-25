732f3fb6d04728b046c2084d11622175ab9a5797
tiagonovaes <tiagonovaesangelo@gmail.com>
2016-04-25 11:46:01 -0300

    ldap

--- agoraunicamp/decorators.py
+++ agoraunicamp/decorators.py
@@ -5,14 +5,29 @@ from django.contrib.auth.models import User as UserSys
 from .models import User, Termo
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+import ldap
+
+
 
 def term_required(function):
+    l = ldap.initialize("ldaps://ldap1.unicamp.br/")
+    baseDN = "ou=people,dc=unicamp,dc=br"
+    searchScope = ldap.SCOPE_SUBTREE
+    retrieveAttributes = None
+    searchFilter = "uid=request.user"
+    ldap_result_id = l.search(baseDN, searchScope, searchFilter, retrieveAttributes)
+    result_type, result_data = l.result(ldap_result_id, 0)
+    l.unbind_s()
+
+    result_data[0][1]['shadowFlag']
+    result_data[0][1]['departmentNumber']
+
     def wrap(request, *args, **kwargs):
         try:
             us = User.objects.get(user=request.user)
         except:
             u = UserSys.objects.get(username=request.user)
-            x = User(user=u, primeiro_nome="x", ultimo_nome="y", projeto="default")
+            x = User(user=u, primeiro_nome=result_data[0][1]['shadowFlag'], ultimo_nome=result_data[0][1]['departmentNumber'], projeto="default")
             x.save()
             us = User.objects.get(user=request.user)
         cond = Termo.objects.get(user=us)
