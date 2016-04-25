1e480cd6312685da6a98011436befcc8127ff176
tiagonovaes <tiagonovaesangelo@gmail.com>
2016-04-25 16:15:34 -0300

    ldap

--- agoraunicamp/decorators.py
+++ agoraunicamp/decorators.py
@@ -22,7 +22,8 @@ def term_required(function):
     result_type, result_data = l.result(ldap_result_id, 0)
     l.unbind_s()
 
-    z = result_data[0][1]['shadowFlag'].values()
+    z = result_data[0][1]['shadowFlag'][0]
+    
     result_data[0][1]['departmentNumber']
 
     def wrap(request, *args, **kwargs):
