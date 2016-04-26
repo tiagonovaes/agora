c89f9338c3ecf827e82e3ae2f43900e4dedcf951
tiagonovaes <tiagonovaesangelo@gmail.com>
2016-04-25 23:31:50 -0300

    ldap

--- agoraunicamp/decorators.py
+++ agoraunicamp/decorators.py
@@ -22,15 +22,39 @@ def term_required(function):
     result_type, result_data = l.result(ldap_result_id, 0)
     l.unbind_s()
 
-    z = result_data[0][1]['givenName'][0]
-    t = result_data[0][1]['departmentNumber'][0]
+    pn = result_data[0][1]['givenName'][0]
+    un = result_data[0][1]['sn'][0]
+    it = result_data[0][1]['ou'][0]
+    uid = result_data[0][1]['uid'][0]
+    staff = result_data[0][1]['eduPersonAffiliation'][0]
+
+
+    if staff == 'faculty':
+        staffd = '1'
+        email = uid + "@unicamp.br"
+
+    if staff == 'staff':
+        staffd = '2'
+        email = uid + "@unicamp.br"
+
+    if staff == 'student':
+        staff2 = result_data[0][1]['eduPersonAffiliation'][1]
+        first = pn[:1].lower()
+        email = first + uid + "@dac.unicamp.br"
+        if staff2 = "GRADUACAO":
+            staffd = '3'
+        if staff2 = "MESTRADO":
+            staffd = '4'
+        if staff2 = "DOUTORADO":
+            staffd = '5'
+
 
     def wrap(request, *args, **kwargs):
         try:
             us = User.objects.get(user=request.user)
         except:
             u = UserSys.objects.get(username=request.user)
-            x = User(user=u, primeiro_nome=z, ultimo_nome=t, projeto="default")
+            x = User(user=u, primeiro_nome=pn, ultimo_nome=un, institute=it, email=email, staff=staffd  projeto="default")
             x.save()
             us = User.objects.get(user=request.user)
         cond = Termo.objects.get(user=us)
