8990877a0fdd4a714eddabde7ebcd0ffcf15a40b
tiagonovaes <tiagonovaesangelo@gmail.com>
2016-04-26 20:32:16 -0300

    ldap

--- agoraunicamp/decorators.py
+++ agoraunicamp/decorators.py
@@ -69,19 +69,17 @@ def term_required(function):
                 email = first + uid + "@dac.unicamp.br"
                 staff2 = result_data[0][1]['eduPersonAffiliation'][1]
                 if staff2 == 'alumni':
-                    try:
-                        staff3 = result_data[0][1]['eduPersonAffiliation'][2]
-                        if staff3 == 'MESTRADO':
-                            staffd == '4'
-                        if staff3 == 'DOUTORADO':
-                            stafdd == '5'
-                    except:
+                    if staff4 == 'none'
                         staffd == '8'
+                    if staff4 == 'MESTRADO':
+                        staffd == '4'
+                    if staff4 == 'DOUTORADO':
+                        stafdd == '5'
                 if staff2 == 'POS-GRADUACAO':
                     staffd = '7'
                 if staff2 == 'GRADUACAO':
                     staffd = '3'
-                    
+
             u = UserSys.objects.get(username=request.user)
             x = User(user=u, primeiro_nome=pn, ultimo_nome=un, institute=it, email=email, staff=staffd, projeto="default")
             x.save()
